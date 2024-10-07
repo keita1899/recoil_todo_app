36eab959ff091feca020bedbb82bdea95d92d40d
keita1899 <saboten18k@gmail.com>
2024-10-07 13:11:17 +0900

    カテゴリー削除機能作成

--- src/components/CategoryCardList.tsx
+++ src/components/CategoryCardList.tsx
@@ -1,14 +1,20 @@
-import { useRecoilValue } from "recoil"
+import { useRecoilState } from "recoil"
 import { categoriesState } from "../atom"
 import { CategoryCard } from "./CategoryCard"
 
 export const CategoryCardList = () => {
-  const categories = useRecoilValue(categoriesState)
+  const [categories, setCategories] = useRecoilState(categoriesState)
+
+  const deleteCategory = (id: number) => {
+    setCategories((oldCategories) => 
+      oldCategories.filter((category) => category.id !== id)
+    )
+  }
 
   return (
     <div className="flex space-x-4 p-4">
       {categories.map((category) => (
-        <CategoryCard key={category.id} category={category} />
+        <CategoryCard key={category.id} category={category} onDeleteCategory={deleteCategory} />
       ))}
     </div>
   )
