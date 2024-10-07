b7e2327a4b58b2cc119ed1d61a3a02d3683deb28
keita1899 <saboten18k@gmail.com>
2024-10-07 23:42:46 +0900

    Todo編集機能作成

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,26 @@
 import { Todo } from "../types"
+import { EditTodoNameForm } from "./EditTodoNameForm"
 import { TodoItem } from "./TodoItem"
 
 type TodoListProps = {
   todos: Todo[]
   onDeleteTodo: (id: number) => void
   onCompleteToggle: (id: number) => void
+  onEditTodoToggle: (id: number) => void
+  onUpdateTodoName: (e: React.FormEvent<HTMLFormElement>,id: number, name: string) => void
 }
 
-export const TodoList = ({todos, onDeleteTodo, onCompleteToggle}: TodoListProps) => {
+export const TodoList = ({todos, onDeleteTodo, onCompleteToggle, onEditTodoToggle, onUpdateTodoName}: TodoListProps) => {
   return (
     <ul className="mt-4">
       {todos.map((todo: Todo) => (
-        <TodoItem key={todo.id} todo={todo} onDeleteTodo={onDeleteTodo} onCompleteToggle={onCompleteToggle} />
+        <>
+          {todo.isEdit ? 
+            <EditTodoNameForm id={todo.id} name={todo.name} onUpdateTodoName={onUpdateTodoName} />
+           :
+            <TodoItem key={todo.id} todo={todo} onDeleteTodo={onDeleteTodo} onCompleteToggle={onCompleteToggle} onEditTodoToggle={onEditTodoToggle} />
+          }
+        </>
       ))}
     </ul>
   )
