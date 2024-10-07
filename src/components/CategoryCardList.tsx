25b5e2d91350add9864fd25df1b770c757007a76
keita1899 <saboten18k@gmail.com>
2024-10-07 13:24:08 +0900

    カテゴリーに紐づくTodoも削除するように修正

--- src/components/CategoryCardList.tsx
+++ src/components/CategoryCardList.tsx
@@ -1,13 +1,17 @@
-import { useRecoilState } from "recoil"
-import { categoriesState } from "../atom"
+import { useRecoilState, useSetRecoilState } from "recoil"
+import { categoriesState, todosState } from "../atom"
 import { CategoryCard } from "./CategoryCard"
 
 export const CategoryCardList = () => {
   const [categories, setCategories] = useRecoilState(categoriesState)
+  const setTodos = useSetRecoilState(todosState)
 
   const deleteCategory = (id: number) => {
     setCategories((oldCategories) => 
       oldCategories.filter((category) => category.id !== id)
+    )
+    setTodos((oldTodos) =>
+      oldTodos.filter((todo) => todo.categoryId !== id)
     )
   }
 
