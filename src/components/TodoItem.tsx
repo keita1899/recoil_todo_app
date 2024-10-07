55c967c5d8e0cfaafaadf439a660004a0b9ae421
keita1899 <saboten18k@gmail.com>
2024-10-07 13:57:45 +0900

    Todo削除機能作成

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,9 +2,10 @@ import { Todo } from "../types"
 
 type TodoProps = {
   todo: Todo
+  onDeleteTodo: (id: number) => void
 }
 
-export const TodoItem = ({todo}: TodoProps) => {
+export const TodoItem = ({todo, onDeleteTodo}: TodoProps) => {
   return (
     <li className="p-3 mb-2 bg-white rounded-lg shadow-sm border border-gray-300 flex justify-between items-center">
       <input
@@ -12,7 +13,7 @@ export const TodoItem = ({todo}: TodoProps) => {
         className="mr-3 h-4 w-4 text-green-500 focus:ring-green-400 border-gray-300 rounded"
       />
       <span>{todo.name}</span>
-      <button className="text-red-500 hover:text-red-700">削除</button>
+      <button onClick={() => onDeleteTodo(todo.id)} className="text-red-500 hover:text-red-700">削除</button>
     </li>
   )
 }
